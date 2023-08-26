83f0ceab6d49e5b5756c1ddd2ba5c849a017534c
Guilherme Barros <guilherme.barros.cunha@gmail.com>
2023-08-25 23:12:23 -0300

    DAWl ATVDD Inicialização no REACT

---
+++ aplha1/src/App.js
@@ -0,0 +1,14 @@
+import Logo from '../src/components/logo/index.js'
+import css from '../src/components/estilo.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="quad">
+        <Logo></Logo>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ aplha1/src/components/logo/index.js
@@ -0,0 +1,11 @@
+import imagem from './image.png'
+
+function logo(){
+    return(
+        <div>
+            <img src={imagem}></img>
+        </div>
+    )
+}
+
+export default logo;
